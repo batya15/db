eb2c1d44030adfe5beb7d97977c58b94b7350863
Batyr Charyev <batya15@mail.ru>
2018-03-02 00:41:54 +0300

    switch space to tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'db-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'db-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  tiles = [
-    {text: 'One', cols: 1, rows: 4, color: 'lightblue'}
-  ];
-
-
+	public tiles: Object = [
+		{text: 'One', cols: 1, rows: 4, color: 'lightblue'}
+	];
 }
