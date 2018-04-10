3d171bf864171cbed092c9f6a61276f2bdf884dc
Charyev Batyr <bcharyev@solit-clouds.com>
2018-04-11 01:28:55 +0300

    test lazyLoading angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,39 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppComponent} from './app.component';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule, MatGridListModule} from '@angular/material';
+import {RouterModule, Routes} from '@angular/router';
 
+const routes: Routes = [
+	{
+		path: 'dashboard',
+		loadChildren: 'app/dashboard/dashboard.module#DashboardModule'
+	},
+	{
+		path: 'admin',
+		loadChildren: 'app/admin/admin.module#AdminModule'
+	},
+	{
+		path: '',
+		redirectTo: '',
+		pathMatch: 'full'
+	}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule.withServerTransition({appId: 'angular-project'}),
-    BrowserAnimationsModule,
-    NoopAnimationsModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatGridListModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+	],
+	imports: [
+		RouterModule.forRoot(routes),
+		BrowserModule.withServerTransition({appId: 'angular-project'}),
+		BrowserAnimationsModule,
+		NoopAnimationsModule,
+		MatButtonModule,
+		MatCheckboxModule,
+		MatGridListModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule {
 }
